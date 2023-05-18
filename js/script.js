59fc661f49179cbd4bfddb4c62dc7d4735c89fc3
RichmanLoveday <lovedayrichman@yahoo.com>
2023-05-18 13:15:14 +0100

    shorty landing page to shorten links

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('err-msg');
+
+btn.addEventListener('click', navToggle);
+linkForm.addEventListener('submit', formSubmit);
+
+
+// Toogle Mobile Menu
+function navToggle(e) {
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+}
+
+
+function validUrl(urlString) {
+    const isValidUrl = urlString => {
+        var urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+                '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+        return !! urlPattern.test(urlString);
+    }
+
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    if (input.value == '') {
+        errMsg.innerHTML = 'Please enter something';
+        input.classList.add('border-red');
+    } else if (! validUrl(input.value)) {
+        errMsg.innerHTML = 'Please enter valid URL';
+        input.classList.add('border-red');
+    } else {
+        errMsg.innerHTML = '';
+        input.classList.remove('border-red');
+        alert('Success');
+    }
+}
